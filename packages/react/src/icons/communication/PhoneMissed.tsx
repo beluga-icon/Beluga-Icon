1e54257999e4bfdbb53c74e98ae026824fdc970e
fatihserhatturan <fatihserhatturan@gmail.com>
2026-04-16 17:40:17 +0300

    Refactor SVG icons for PhoneMissed, PhoneOff, Fingerprint, and Strikethrough for improved structure and readability

--- packages/react/src/icons/communication/PhoneMissed.tsx
+++ packages/react/src/icons/communication/PhoneMissed.tsx
@@ -12,14 +12,17 @@ export const PhoneMissedMeta: IconMeta = {
   version: '0.1.0',
 }
 
-export const PhoneMissedIcon = forwardRef<SVGSVGElement, IconProps>(function PhoneMissedIcon(props, ref) {
-  return (
-    <Icon ref={ref} {...props}>
-      <polyline points="23 1 16 1 16 8"/>
-  <line x1="23" y1="1" x2="14" y2="10"/>
-  <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 5 9.81a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 1.06 1h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L5.09 8.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"/>
-    </Icon>
-  )
-})
+export const PhoneMissedIcon = forwardRef<SVGSVGElement, IconProps>(
+  function PhoneMissedIcon(props, ref) {
+    return (
+      <Icon ref={ref} {...props}>
+        <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 13a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.6 2.18h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L7.91 9.9a16 16 0 0 0 6.29 6.29l.9-.9a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
+
+        <line x1="23" y1="1" x2="17" y2="7" />
+        <polyline points="17 3 17 7 21 7" />
+      </Icon>
+    )
+  },
+)
 
 PhoneMissedIcon.displayName = 'PhoneMissedIcon'
--- packages/react/src/icons/ui/Fingerprint.tsx
+++ packages/react/src/icons/ui/Fingerprint.tsx
@@ -12,18 +12,18 @@ export const FingerprintMeta: IconMeta = {
   version: '0.1.0',
 }
 
-export const FingerprintIcon = forwardRef<SVGSVGElement, IconProps>(function FingerprintIcon(props, ref) {
-  return (
-    <Icon ref={ref} {...props}>
-      <path d="M2 13.5V8a10 10 0 0 1 20 0v5.5"/>
-  <path d="M6 12a6 6 0 0 1 12 0v2"/>
-  <path d="M10 12a2 2 0 0 1 4 0v8"/>
-  <path d="M2 16a2 2 0 0 0 2 2h1"/>
-  <path d="M21 14a2 2 0 0 1-2 2h-1"/>
-  <path d="M2 19a10 10 0 0 0 4 2"/>
-  <path d="M18 21a10 10 0 0 0 4-2"/>
-    </Icon>
-  )
-})
+export const FingerprintIcon = forwardRef<SVGSVGElement, IconProps>(
+  function FingerprintIcon(props, ref) {
+    return (
+      <Icon ref={ref} {...props}>
+        <path d="M2 21q10-3 20 0" />
+        <path d="M3 18q9-5 18 0" />
+        <path d="M5 15q7-6 14 0" />
+        <path d="M7 12q5-7 10 0" />
+        <path d="M10 9q2-5 4 0" />
+      </Icon>
+    )
+  },
+)
 
 FingerprintIcon.displayName = 'FingerprintIcon'
